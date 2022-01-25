f8f3918201994ef50a2eb2dce30033d0637de55e
Josh Dimpas <40543984+JoshCunningHum@users.noreply.github.com>
2022-01-26 01:04:12 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+
+// use the express-static middleware
+app.use(express.static("public"))
+
+// start the server listening for requests
+app.listen(process.env.PORT || 3001, 
+	() => console.log("Server is running..."));
