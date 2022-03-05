22359e681a1c4ad8fe0b5ca744d319652c0e334c
Nathaniel Tampus <hello@nize.ph>
2022-03-05 21:27:59 +0800

    just use global properties directly instead of using functions

--- src/routes/session.js
+++ src/routes/session.js
@@ -7,7 +7,7 @@ const router = Router();
 router.post('/', async function(req, res) {
     if(req.session.passport) {
         try {
-            const user = await global.getUsers().find(users => users.id === req.session.passport.user);
+            const user = await global.users.find(users => users.id === req.session.passport.user);
             if (!user) throw new Error('User not found');
             res.json(user);
         } catch (error) {
